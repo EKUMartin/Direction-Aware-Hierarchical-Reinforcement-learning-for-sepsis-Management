f244dd714d65981e8f05325fe7a5a7a251c59165
EKUmartin <movieunknown85@gmail.com>
2026-09-08 16:36:41 +0900

    no message

---
+++ Pretraining/pretraining.py
@@ -0,0 +1,35 @@
+# sampling 1000 stayids for pretraining
+class sample:
+    def __init__(self,data,iterations,threshold):
+        # initial variables
+        self.data=data
+        self.iteratons=iterations
+        self.threhold=threshold
+        # population
+        self.population_sofamax=[]
+        self.population_sofamin=[]
+        self.population_sofarange=[]
+        # sample
+        self.sample_sofamax=[]
+        self.sample_sofamin=[]
+        self.sample_sofarange=[]
+    def get_sample(self):
+
+    def get_sofa_max(self):
+
+    def get_sofa_min(self):
+
+    def get_sofa_range(self):
+
+    def get_wasserstein_distance(self):
+
+    def main(self):
+    
+
+
+
+# Pretraining HMM
+
+
+
+# Pretraining Encoder
