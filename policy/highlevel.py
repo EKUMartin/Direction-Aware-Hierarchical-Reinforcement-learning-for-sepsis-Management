5624693b9e3d4aa20a59788af15ddc72cf984e6f
EKUmartin <movieunknown85@gmail.com>
2026-09-18 16:10:13 +0900

    하이레벨, 로우레벨

--- policy/highlevel.py
+++ policy/highlevel.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 
 class high_level_target(nn.Module):
     def __init__(self, input_size=3,output_size=1):
+        super().__init__()
         self.layer=nn.Sequential(nn.Linear(input_size,256),
                                  nn.SiLU(),
                                  nn.Linear(128,64),
@@ -14,6 +15,7 @@ class high_level_target(nn.Module):
                                  )
         self.spectlayer=nn.Linear(32,output_size)
         self.memory=[]
+
     def forward(self,x):
         x=self.layer(x)
         nn.utils.parametrizations.spectral_norm(self.spectlayer,name="weight")
@@ -22,6 +24,7 @@ class high_level_target(nn.Module):
 
 class high_level_local(nn.Module):
     def __init__(self, input_size=3,output_size=1):
+        super().__init__()
         self.layer=nn.Sequential(nn.Linear(input_size,256),
                                  nn.SiLU(),
                                  nn.Linear(128,64),
@@ -31,6 +34,7 @@ class high_level_local(nn.Module):
                                  )
         self.spectlayer=nn.Linear(32,output_size)
         self.memory=[]
+
     def forward(self,x):
         x=self.layer(x)
         nn.utils.parametrizations.spectral_norm(self.spectlayer,name="weight")
--- policy/lowlevel.py
+++ policy/lowlevel.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 
 class low_level_actor(nn.Module):
     def __init__(self):
+        super().__init__()
         self.linear= nn.Sequential()
 
     def forward(self,x):
